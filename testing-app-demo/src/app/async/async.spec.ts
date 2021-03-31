6ae1dc51205c8e33d36587c0e3df215fb3e524a4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-31 12:25:58 +0530

    Async testing - Promises & setTimeout

--- testing-app-demo/src/app/async/async.spec.ts
+++ testing-app-demo/src/app/async/async.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, flush } from '@angular/core/testing';
+import { fakeAsync, flush, flushMicrotasks, tick } from '@angular/core/testing';
 
 describe("Async tests", () => {
     it("testing setTimeout", (done) => {
@@ -24,6 +24,54 @@ describe("Async tests", () => {
         flush()
         expect(test1).toBeTrue()
         expect(test2).toBeFalse()
+    }))
+
+    it("testing promises", fakeAsync(() => {
+        let test = false;
+        console.log('Creating a promise');
+
+        Promise
+            .resolve()
+            .then(() => {
+                console.log('First "then" executed successfully');
+                return Promise.resolve();
+            })
+            .then(() => {
+                console.log('Second "then" executed successfully');
+                setTimeout(function () {
+                    test = true;
+                })
+            });
+       // flushMicrotasks();
+        flush()
+        console.log('Running test assertions');
+        expect(test).toBeTrue()
+    }));
+
+    it("Async testing - Promises & setTimeout", fakeAsync(() => {
+        let counter = 0;
+
+        Promise.resolve()
+            .then(() => {
+                counter += 10;
+                setTimeout(() => {
+                    counter += 1;
+                }, 1000)
+            });
+        
+        expect(counter).toBe(0);
+
+        flushMicrotasks();
+        
+        expect(counter).toBe(10);
+        
+        tick(500);
+
+        expect(counter).toBe(10);
+
+        tick(500);
+
+        expect(counter).toBe(11);
     }))
 })
 
